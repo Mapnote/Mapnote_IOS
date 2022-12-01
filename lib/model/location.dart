e31ba152213dbb90611fea413140a1a72a8ec31b
seungmi <seungmi98@khu.ac.kr>
2022-12-02 00:41:31 +0900

    Update location.dart

--- lib/model/location.dart
+++ lib/model/location.dart
@@ -5,5 +5,5 @@ class LocationClass extends LatLng {
   final double latitude;
   final double longitude;
 
-  const LocationClass({@required this.latitude, @required this.longitude}) : super(latitude, longitude);
+  const LocationClass({required this.latitude, required this.longitude}) : super(latitude, longitude);
 }
